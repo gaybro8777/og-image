5c3520706306cc717517ebdb16f7bdeb86aad333
Peer Richelsen <peeroke@richelsen.net>
2021-05-11 10:55:03 +0100

    wip

--- web/index.ts
+++ web/index.ts
@@ -142,16 +142,23 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'Calendso', value: 'https://calendso.com/calendso-logo.svg'},
+    { text: 'Vercel', value: 'https://calendso.com/calendso-logo-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
-
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+  { text: "Vercel", value: "https://calendso.com/calendso-logo.svg" },
+  {
+    text: "Next.js",
+    value:
+      "https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg",
+  },
+  {
+    text: "Hyper",
+    value:
+      "https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg",
+  },
 ];
 
 const widthOptions = [
